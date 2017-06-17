d0b4605940342ffba5a024ceb3c02b8408c819b6
Shcherbachenko <shcherbachenko.sergey@gmail.com>
2017-06-17 23:26:47 +0300

    AWS init commit

---
+++ handler.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports.exchage_avg_day = (event, context, callback) => {
+	console.log("ddd");
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
