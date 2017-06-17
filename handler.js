5c9b1514f94ba776aae173005825561fc08b440c
Shcherbachenko <shcherbachenko.sergey@gmail.com>
2017-06-18 01:29:23 +0300

    add lambda function

--- handler.js
+++ handler.js
@@ -1,9 +1,30 @@
 'use strict';
-
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient({region: "eu-central-1"});
 module.exports.exchage_avg_day = (event, context, callback) => {
-	console.log("ddd");
-  callback(null, response);
+	let toDay = new Date();
+	let before_date = toDay.setDate(toDay.getDate() + 1);
+	let after_date = toDay.setDate(toDay.getDate() - 1);
+	var params = {
+		TableName : "currency",
+		ProjectionExpression:"#date, currencies",
+		FilterExpression: "#date between  :after_date and :before_date",
+		ExpressionAttributeNames:{
+			"#date": "date"
+		},
+		ExpressionAttributeValues: {
+			":before_date": before_date,
+			":after_date": after_date
+		}
+	};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+	docClient.scan(params, function(err, data) {
+		if (err) {
+			callback(null, JSON.stringify(err, null, 2));
+		} else {
+			console.log("Query succeeded.");
+			console.log(data.Items)
+			callback(null, data);
+		}
+	});
 };
