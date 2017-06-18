69c4d7719a7220682f01c10a2e231dc93c2ba0e9
vtrushin <monospectr@gmail.com>
2017-06-18 11:55:01 +0300

    Added aws-sdk

--- handler.js
+++ handler.js
@@ -1,22 +1,21 @@
 'use strict';
-const AWS = require("aws-sdk");
-const docClient = new AWS.DynamoDB.DocumentClient({ region: "eu-central-1" });
-const updateTables = require('update-tables');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-central-1' });
 
 function exchage_avg_day(event, context, callback) {
 	const today = new Date();
 	const startDate = today.setDate(today.getDate() + 1);
 	const endDate = today.setDate(today.getDate() - 1);
 	const params = {
-		TableName : "currency",
-		ProjectionExpression:"#date, currencies",
-		FilterExpression: "#date between :after_date and :before_date",
+		TableName: 'currency',
+		ProjectionExpression: '#date, currencies',
+		FilterExpression: '#date between :after_date and :before_date',
 		ExpressionAttributeNames: {
-			"#date": "date"
+			'#date': 'date'
 		},
 		ExpressionAttributeValues: {
-			":before_date": startDate,
-			":after_date": endDate
+			':before_date': startDate,
+			':after_date': endDate
 		}
 	};
 
@@ -24,7 +23,7 @@ function exchage_avg_day(event, context, callback) {
 		if (err) {
 			callback(null, JSON.stringify(err, null, 2));
 		} else {
-			console.log("Query succeeded. 11");
+			console.log('Query succeeded. 11');
 			console.log(data.Items);
 			callback(null, data);
 		}
@@ -32,4 +31,3 @@ function exchage_avg_day(event, context, callback) {
 }
 
 module.exports.exchage_avg_day = exchage_avg_day;
-module.exports.updateTables = updateTables;
